fb2d3dc98271d3a7a355863ed659bb8d067efa62
Simon.Z <zhanghan.simon@gmail.com>
2022-10-18 16:54:40 -0700

    fix the stat issue

--- src/api/controllers/stat.controller.ts
+++ src/api/controllers/stat.controller.ts
@@ -7,12 +7,12 @@ import { BaseController } from './base.controller';
 
 export class StatController extends BaseController {
   constructor(rootLogger: Logger) {
-    super(rootLogger, 'deposit', '/api/stat');
+    super(rootLogger, 'stat', '/api/stat');
     this.router.get(`${this.path}/`, try$(this.getStat));
   }
 
   private getStat = async (req: Request, res: Response) => {
-    let result = [];
+    let result = {};
     for (const fromChain of chainConfigs) {
       const home = fromChain.domainId;
       for (const toChain of chainConfigs) {
@@ -23,6 +23,7 @@ export class StatController extends BaseController {
 
         const dc = await this.depositRepo.countByHomeAndDest(home, dest);
         const pc = await this.proposalRepo.countByHomeAndDest(home, dest);
+        this.log.info({ home, dest, dc, pc }, 'counted on network');
         if (dc > 0 || pc > 0) {
           result[`${Network[fromChain.network]} -> ${Network[toChain.network]}`] = { deposit: dc, proposal: pc };
         }
