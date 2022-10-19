4dc6e13cdca2dae642981a89ec380953f899c89d
Simon.Z <zhanghan.simon@gmail.com>
2022-10-19 16:18:17 -0700

    add history api

--- src/api/controllers/deposit.controller.ts
+++ src/api/controllers/deposit.controller.ts
@@ -1,3 +1,5 @@
+import { resourceLimits } from 'worker_threads';
+
 import { Request, Response } from 'express';
 import { Logger } from 'pino';
 
@@ -9,6 +11,7 @@ export class DepositController extends BaseController {
   constructor(rootLogger: Logger) {
     super(rootLogger, 'deposit', '/api/deposit');
     this.router.get(`${this.path}/:home/:dest/:nonce`, try$(this.getDepositByKey));
+    this.router.get(`${this.path}/history/:address`, try$(this.getHistoryDeposits));
   }
 
   getDepositByKey = async (req: Request, res: Response) => {
@@ -40,5 +43,39 @@ export class DepositController extends BaseController {
       sigPassTx: spass?.txHash,
       proposalTxs: proposal?.txHashs,
     });
+  };
+
+  getHistoryDeposits = async (req: Request, res: Response) => {
+    const { address } = req.params;
+    const deposits = await this.depositRepo.findByAddress(address);
+    if (!deposits || deposits.length < 0) {
+      return res.json({ address, status: 'deposit not found' });
+    }
+    const result = [];
+    for (const deposit of deposits) {
+      let status = 'created';
+      const sigs = await this.submitSignatureRepo.findByKey(deposit.key);
+      const nsig = sigs.length;
+      if (nsig > 0) {
+        status = 'voted';
+      }
+      const spass = await this.signaturePassRepo.findByKey(deposit.key);
+      if (spass) {
+        status = 'passed';
+      }
+      const proposal = await this.proposalRepo.findByKey(deposit.key);
+      if (proposal) {
+        status = ProposalStatus[proposal.status];
+      }
+      result.push({
+        ...deposit.toJSON(),
+        status,
+        votedRelayer: sigs.map((s) => s.relayerAddr),
+        voteTxs: sigs.map((s) => s.txHash),
+        sigPassTx: spass?.txHash,
+        proposalTxs: proposal?.txHashs,
+      });
+    }
+    return res.json(result);
   };
 }
